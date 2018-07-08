cca4c7c83985e2136731c1e45e6e0641c67f9b99
Matt Tilley <mtilley255@gmail.com>
2018-07-08 09:55:09 -0500

    End of project commit.

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
+ /*  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBtZzY8yyAy2gEKTquWhFDwMZeBGCYXBQU",
+    authDomain: "contactform-f82aa.firebaseapp.com",
+    databaseURL: "https://contactform-f82aa.firebaseio.com",
+    projectId: "contactform-f82aa",
+    storageBucket: "contactform-f82aa.appspot.com",
+    messagingSenderId: "254956803548"
+  }; */
+
+  firebase.initializeApp(config);
+
+
+
+// Reference messages collection
+
+let messagesRef = firebase.database().ref("messages");
+
+
+
 /* Listen for form submit */
 
 document.getElementById("contactForm").addEventListener("submit", submitForm);
 
+
+
+// Submit form
 function submitForm(e){
 
     e.preventDefault();
@@ -12,16 +35,43 @@ function submitForm(e){
     let phone = getInputVal("phone");
     let message = getInputVal("message");
 
-    console.log(name);
-    console.log(company);
-    console.log(email);
-    console.log(phone);
-    console.log(message);
+    // Save message
+    saveMessage(name, company, email, phone, message);
+
+    // Show alert
+    document.querySelector(".alert").style.display = "block";
+
+    // Hide alert after 3 seconds.
+    setTimeout(function(){
+        document.querySelector(".alert").style.display = "none";
+    },3000);
+
+    // Clear form
+    document.getElementById("contactForm").reset();  
+
 }
+
 
 
 /* Function to get form values */
 
 function getInputVal(id){
     return document.getElementById(id).value;
+}
+
+
+
+// Save message to FireBase
+function saveMessage(name, company, email, phone, message){
+    
+    let newMessagesRef = messagesRef.push();
+
+    newMessagesRef.set({
+        name: name,
+        company: company,
+        email: email,
+        phone: phone,
+        message: message
+    });
+
 }
